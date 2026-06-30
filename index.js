94ab99baf905845bf3db7465fbe57911ae4631fd
Gumdrop <david.nyongesa@student.moringaschool.com>
2026-06-30 21:39:47 +0300

    functions done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"]
+const featuredDrink = "Strawberry Milkshake"
+
+function addBurger () {
+    let newBurger = "Flatburger"
+    burgers.push(newBurger)
+}
+if(true) {
+        let anotherNewBurger = "Maple Bacon Burger"
+        burgers.push(anotherNewBurger)
+}
+
+function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake"
+}
